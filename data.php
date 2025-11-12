babc3c757a02baf2577ed327f7b755dfb7d984f0
Anton Börnfelt <antonb@MacBook-Pro-som-tillhor-Anton.local>
2025-11-12 11:40:41 +0100

    big big changes

---
+++ data.php
@@ -0,0 +1,63 @@
+<?php
+$films = [
+    [
+        'title' => 'Bugonia',
+        'genre' => 'Thriller',
+        'poster' => '625400-bugonia-0-2000-0-3000-crop.jpg'
+    ],
+    [
+        'title' => 'One Battle After Another',
+        'genre' => 'Action',
+        'poster' => '951277-one-battle-after-another-0-2000-0-3000-crop.jpg'
+    ],
+    [
+        'title' => 'Frankenstein',
+        'genre' => 'Horror',
+        'poster' => '958100-frankenstein-2025-0-2000-0-3000-crop.jpg'
+    ],
+    [
+        'title' => 'Regretting You',
+        'genre' => 'Drama',
+        'poster' => '1208076-regretting-you-0-2000-0-3000-crop.jpg'
+    ],
+    [
+        'title' => 'Springsteen: Deliver Me from Nowehere',
+        'genre' => 'Music',
+        'poster' => '1113868-springsteen-deliver-me-from-nowhere-0-2000-0-3000-crop.jpg'
+    ],
+    [
+        'title' => 'TRON: Ares',
+        'genre' => 'Action',
+        'poster' => '462903-tron-ares-0-2000-0-3000-crop.jpg'
+    ],
+    [
+        'title' => 'Good Fortune',
+        'genre' => 'Comedy',
+        'poster' => '1005018-good-fortune-2025-0-2000-0-3000-crop.jpg'
+    ],
+    [
+        'title' => 'Citizen Kane',
+        'genre' => 'Drama',
+        'poster' => '2702-citizen-kane-0-2000-0-3000-crop.jpg'
+    ],
+    [
+        'title' => 'Nuremberg',
+        'genre' => 'Drama',
+        'poster' => '1098782-nuremberg-2025-0-2000-0-3000-crop.jpg'
+    ],
+    [
+        'title' => 'The Running Man',
+        'genre' => 'Action',
+        'poster' => '716178-the-running-man-2025-0-2000-0-3000-crop.jpg'
+    ],
+    [
+        'title' => 'Preadator: Badlands',
+        'genre' => 'Action',
+        'poster' => '1125809-predator-badlands-0-2000-0-3000-crop.jpg'
+    ],
+    [
+        'title' => 'Anniversary',
+        'genre' => 'Horror',
+        'poster' => '1015194-anniversary-2025-0-2000-0-3000-crop.jpg'
+    ],
+];
