23e1ead70a05551fea62b1198898e282aae24082
Hanna Johansson <hannajohansson16@hotmail.com>
2025-11-30 16:37:49 +0100

    updated code, added socials imgs

--- footer-new.php
+++ footer-new.php
@@ -2,5 +2,9 @@
 
     <div class="footer_text">Contact | Newsletter</div>
     <div class="footer_text">© FilmTastic 2025</div>
+    <!-- <div class="footer_icons">
+        <img class="icon" src="resources-detail-page/icon_facebook.png">
+        <img class="icon" src="resources-detail-page/icon_insta.png">
+    </div> -->
 
 </section>
